037ea785f1a448328600d0403aa6c75ed1966a27
greggaka <greggakamine@gmail.com>
2022-09-27 12:16:17 -1000

    ninja bonus, added class method

--- Python/fundamentals/oop/bank_account.py
+++ Python/fundamentals/oop/bank_account.py
@@ -2,12 +2,19 @@ class BankAccount:
     #class attributes
     interest_rate = .02 
     balance = 0
-    account_info =[]
+    all_accounts =[]
     
-    #instance attributes
+    #self attributes
     def __init__(self, int_rate, balance):
         self.int_rate = int_rate
         self.balance = balance
+        BankAccount.all_accounts.append(self)
+        
+    #self methods
+    def account_info (self):
+        print(f"Interest Rate: {self.int_rate}")
+        print(f"Balance: {self.balance}")
+        return self
     
     def deposit(self, amount):
         self.balance += amount
@@ -30,11 +37,15 @@ class BankAccount:
             self.balance += self.balance * self.int_rate
         return self 
     
-    # @classmethod
-    # def bank_account_info(cls)
+    @classmethod
+    def bank_account_info(cls):
+        for account in cls.all_accounts:
+            print(account.account_info())
 
 account1 = BankAccount (.04, 100)
 account2 = BankAccount (.05, 1000)
 
 account1.deposit(100).deposit(100).deposit(100).withdraw(25).yield_interest().display_account_info() #output: $390.00
 account2.deposit(1000).deposit(1000).withdraw(125).withdraw(125).withdraw(125).withdraw(125).yield_interest().display_account_info() #output: 2625.00
+
+BankAccount.bank_account_info()
